b84db4cec88039df188bf6701cee4a1b73655f71
webtonyy <antoniofeolsa@gmail.com>
2024-07-17 18:45:49 -0300

    Moved databases from sqlite to mysql using docker

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import pymysql
+
+conn = pymysql.connect(
+    host = '127.0.0.1',
+    port = 3306,
+    user = 'root',
+    passwd = 'admin123',
+    db = 'mysql'
+)
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud'
 login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
